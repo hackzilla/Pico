b50ed4c5a8d4ad327c4cd45f0197020358b70b1e
Daniel Platt <dan@ukwebmedia.com>
2012-05-09 22:23:46 +0100

    link homepage to layout template

--- src/Ofdan/SearchBundle/Controller/StaticController.php
+++ src/Ofdan/SearchBundle/Controller/StaticController.php
@@ -7,6 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class StaticController extends Controller
 {
+    public function homeAction()
+    {
+        return $this->render('OfdanSearchBundle::layout.html.twig');
+    }
+
     public function aboutAction()
     {
         return $this->render('OfdanSearchBundle:Static:about.html.twig');
