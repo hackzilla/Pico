c29943dda71767afb57f30cdc2cd18d807cf5820
Daniel Platt <dan@ukwebmedia.com>
2012-05-18 17:18:00 +0100

    add data fixtures for Languages

---
+++ src/Ofdan/SearchBundle/DataFixtures/ORM/LanguageDataFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Ofdan\SearchBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Ofdan\SearchBundle\Entity\Language;
+
+class LanguageFixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $language1 = new Language();
+        $language1->setCc('en');
+        $language1->setNameEng('English');
+        $language1->setCreated(new \DateTime());
+        $manager->persist($language1);
+
+        $language2 = new Language();
+        $language2->setCc('fr');
+        $language2->setNameEng('French');
+        $language2->setCreated(new \DateTime());
+        $manager->persist($language2);
+
+        $language3 = new Language();
+        $language3->setCc('de');
+        $language3->setNameEng('German');
+        $language3->setCreated(new \DateTime());
+        $manager->persist($language3);
+
+        $language4 = new Language();
+        $language4->setCc('es');
+        $language4->setNameEng('Spanish');
+        $language4->setCreated(new \DateTime());
+        $manager->persist($language4);
+
+        $language5 = new Language();
+        $language5->setCc('pt');
+        $language5->setNameEng('Portuguese');
+        $language5->setCreated(new \DateTime());
+        $manager->persist($language5);
+        
+        $manager->flush();
+    }
+
+}
