8fdcca0c828276298367e218de8cba996dbcaae7
Daniel Platt <dan@ukwebmedia.com>
2012-06-05 14:42:16 +0100

    add placeholder statistics page

--- src/Ofdan/SearchBundle/Controller/PageController.php
+++ src/Ofdan/SearchBundle/Controller/PageController.php
@@ -17,6 +17,29 @@ class PageController extends Controller
         ));
     }
     
+    public function statisticsAction()
+    {
+        
+        return $this->render('OfdanSearchBundle:Page:statistics.html.twig', array(
+            'average_seek' => 0,
+            'total_queries' => 0,
+            'total_queries_by_day' => 0,
+            'known_lang' => 0,
+            'known_words' => 0,
+            'min_word_length' => 0,
+            'avg_word_length' => 0,
+            'max_word_length' => 0,
+            'known_domains' => 0,
+            'cached_domains' => 0,
+            'stored_robots' => 0,
+            'blocked_domains' => 0,
+            'queued_domains' => 0,
+            'require_update_domains' => 0,
+            'disk_storage' => 0,
+            'processor_load' => 0,
+        ));
+    }
+    
     public function spyAction()
     {
         $repository = $this->getDoctrine()
