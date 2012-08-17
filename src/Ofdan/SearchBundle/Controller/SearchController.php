9efff3dbf151b89e84790c1ff759dc0de97b1987
Daniel Platt <dan@ukwebmedia.com>
2012-08-17 22:08:49 +0100

    add did you mean feature

--- src/Ofdan/SearchBundle/Controller/SearchController.php
+++ src/Ofdan/SearchBundle/Controller/SearchController.php
@@ -20,8 +20,39 @@ class SearchController extends Controller
 
         return $this->render('OfdanSearchBundle:Search:results.html.twig', array(
             'results' => $this->getResults($query),
+            'suggestion' => $this->getSuggestion($query),
             'query' => $query,
         ));
+    }
+
+    public function getSuggestion($query)
+    {
+        $words = preg_split('/[ ,;.]/', $query );
+
+        $incorrectWords = array();
+        $int = pspell_new('en_GB-w-accents');
+
+        if( $int )
+        {
+            foreach( $words as $word ) {
+
+                if( !pspell_check( $int, $word ) ) {
+                    $suggest = pspell_suggest($int, $word);
+                    if (isset($suggest[0])) {
+                            $incorrectWords[$word] = '<strong>'.$suggest[0].'</strong>';
+                    }
+                }
+            }
+
+            $correctWords = array_values( $incorrectWords );
+            $incorrectWords = array_keys( $incorrectWords );
+
+            $checkedStr = str_replace( $incorrectWords, $correctWords, $query );
+        } else {
+                $checkedStr = NULL;
+        }
+
+        return $checkedStr;
     }
 
     public function getResults($query)
