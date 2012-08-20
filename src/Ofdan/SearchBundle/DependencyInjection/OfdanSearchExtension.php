021d4186227f127221123ab1e4dbfa8153288194
Daniel Platt <dan@ukwebmedia.com>
2012-08-20 22:13:45 +0100

    change services file to xml. add service for results

--- src/Ofdan/SearchBundle/DependencyInjection/OfdanSearchExtension.php
+++ src/Ofdan/SearchBundle/DependencyInjection/OfdanSearchExtension.php
@@ -23,8 +23,8 @@ class OfdanSearchExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
         $definition = new Definition('Ofdan\SearchBundle\Extension\IpTwigExtension');
         // this is the most important part. Later in the startup process TwigBundle
