8eb9a0fb640cbaebe5f7dd9bafd601569b92f7a7
Daniel Platt <dan@ukwebmedia.com>
2012-09-01 21:57:41 +0100

    store query in broken form

--- src/Ofdan/SearchBundle/Service/Results.php
+++ src/Ofdan/SearchBundle/Service/Results.php
@@ -6,15 +6,32 @@ class Results
 {
     protected $max_results;
     protected $results_per_page;
+
+    protected $queryAny = array();
+    protected $queryRequired = array();
+    protected $queryExclude = array();
+
     public function __construct($max_results, $results_per_page)
     {
         $this->max_results = $max_results;
         $this->results_per_page = $results_per_page;
     }
 
-    public function setQuery()
+    public function setQuery($queryStr)
     {
-        return array();
+        $words = explode(' ', $queryStr);
+        
+        foreach($words as $word) {
+            if($word) {
+                if('-' == $word[0]) {
+                    $this->queryExclude = substr($word, 1);
+                } else if('+' == $word[0]) {
+                    $this->queryRequired = substr($word, 1);
+                } else {
+                    $this->queryAny = $word;
+                }
+            }
+        }
     }
 
     public function getResults()
