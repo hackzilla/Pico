db698804a3e705c3326079c417abf06facbe98d9
Daniel Platt <dan@ukwebmedia.com>
2012-09-04 22:29:48 +0100

    placeholder controller for cache displaying

---
+++ src/Ofdan/SearchBundle/Controller/CacheController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ofdan\SearchBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class CacheController extends Controller
+{  
+    public function indexAction()
+    {
+        
+    }
+}
