42151328ef6bc8b70fc9d0264646be410d78ad10
Daniel Platt <dan@ukwebmedia.com>
2012-09-04 22:08:58 +0100

    add more metadata row for data fixtures

--- src/Ofdan/SearchBundle/DataFixtures/ORM/MetaDataDataFixtures.php
+++ src/Ofdan/SearchBundle/DataFixtures/ORM/MetaDataDataFixtures.php
@@ -18,6 +18,27 @@ class MetadataFixtures extends AbstractFixture implements OrderedFixtureInterfac
         $metadata1->setExtract('The initial installation of Debian Apache.');
         $manager->persist($metadata1);
 
+        $metadata2 = new Metadata();
+        $metadata2->setDomain($manager->merge($this->getReference('github-domain')));
+        $metadata2->setLang('en');
+        $metadata2->setDialect('us');
+        $metadata2->setExtract('The initial installation of Debian Apache.');
+        $manager->persist($metadata2);
+
+        $metadata3 = new Metadata();
+        $metadata3->setDomain($manager->merge($this->getReference('slashdot-domain')));
+        $metadata3->setLang('en');
+        $metadata3->setDialect('us');
+        $metadata3->setExtract('The initial installation of Debian Apache.');
+        $manager->persist($metadata3);
+
+        $metadata4 = new Metadata();
+        $metadata4->setDomain($manager->merge($this->getReference('open-domain')));
+        $metadata4->setLang('en');
+        $metadata4->setDialect('us');
+        $metadata4->setExtract('The initial installation of Debian Apache.');
+        $manager->persist($metadata4);
+
         $manager->flush();
     }
 
