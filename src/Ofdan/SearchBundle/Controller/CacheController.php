95e4c35a8a38cc300a50a1dd34ba0375cfd0dbca
Daniel Platt <git@ofdan.co.uk>
2012-09-23 13:52:10 +0100

    Display Caches for search results

--- src/Ofdan/SearchBundle/Controller/CacheController.php
+++ src/Ofdan/SearchBundle/Controller/CacheController.php
@@ -9,6 +9,29 @@ class CacheController extends Controller
 {  
     public function indexAction()
     {
+        $request = $this->getRequest();
+        $domain = $request->query->get('domain');
         
+        if(null !== $domain) {
+            $em = $this->getDoctrine()
+                        ->getEntityManager();
+
+            $domainCache = $em->getRepository('OfdanSearchBundle:CacheIndex')
+                ->getDomainCache($domain);
+            
+            if(null !== $domainCache) {
+                return $this->render('OfdanSearchBundle:Cache:view.html.twig', array(
+                    'domain' => $domain,
+                    'base_url' => 'http://'.$domain.'/',
+                    'cache' => $domainCache,
+                ));
+            } else {
+                return $this->render('OfdanSearchBundle:Cache:no_cache.html.twig', array(
+                    'domain' => $domain,
+                ));
+            }
+        } else {
+            return $this->render('OfdanSearchBundle:Cache:no_domain.html.twig');
+        }
     }
 }
--- src/Ofdan/SearchBundle/Repository/CacheIndexRepository.php
+++ src/Ofdan/SearchBundle/Repository/CacheIndexRepository.php
@@ -3,6 +3,7 @@
 namespace Ofdan\SearchBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Ofdan\SearchBundle\Entity\Domain;
 
 class CacheIndexRepository extends EntityRepository
 {
@@ -14,5 +15,34 @@ class CacheIndexRepository extends EntityRepository
         
         return $qb->getQuery()
                 ->getSingleResult();
+    }
+    
+    public function getDomainCache($domain)
+    {
+        $qb = $this->createQueryBuilder('c')
+                ->select('c')
+                ->from('\Ofdan\SearchBundle\Entity\Domain', 'd')
+                ->where('d.domain = :domain')
+                ->andWhere('d.status = :status')
+                ->andWhere('d.id = c.domain')
+                ->setParameter('domain', $domain)
+                ->setParameter('status', Domain::STATUS_STORED)
+        ;
+
+        $query = $qb->getQuery();
+
+        try {
+            $cache = $query->getResult();
+            
+            if(!empty($cache)) {
+                $cache = $cache[0];
+            } else {
+                $cache = NULL;
+            }
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            $cache = NULL;
+        }
+
+        return $cache;
     }
 }
