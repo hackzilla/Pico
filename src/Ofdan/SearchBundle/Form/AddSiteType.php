1f3d33a8fe07dd0626a9651a4b400f266c869ca7
Daniel Platt <github@ofdan.co.uk>
2012-09-25 21:44:57 +0100

    convert form to form builder interface

--- src/Ofdan/SearchBundle/Form/AddSiteType.php
+++ src/Ofdan/SearchBundle/Form/AddSiteType.php
@@ -3,11 +3,11 @@
 namespace Ofdan\SearchBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class AddSiteType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('domain', 'text');
         $builder->add('captcha', 'captcha');
